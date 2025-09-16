0c236c591b623aa2b21db92db3d9ac9d09861778
Akash802980 <71087124+Akash802980@users.noreply.github.com>
2025-09-16 21:54:20 +0530

    Update nxt.py

--- nxt.py
+++ nxt.py
@@ -4,9 +4,9 @@ from urllib.parse import urlparse, parse_qs
 from datetime import datetime
 
 # Sources
-json_url = "https://alinonereborn.fun/jstrweb2/index.php"
+json_url = "https://allinonereborn.fun/jstrweb2/index.php"
 new_backup_url = "https://jt.drmlive.net/jiotvplus/156.mpd"
-backup_url = "https://game.enver1769.fun/Jtv/VPifZa/Jtv.mpd?id=143"
+backup_url = "https://game.denver1769.fun/Jtv/VPifZa/Jtv.mpd?id=143"
 zee_m3u_url = "https://raw.githubusercontent.com/alex8875/m3u/refs/heads/main/z5.m3u"
 
 # Playlist file
